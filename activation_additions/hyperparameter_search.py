3343a0644ce481a4ce485a369cafcb4ad49a7133
wuschel1 <wuschelschulz8@gmail.com>
2023-07-19 00:57:05 +0000

    I am ading typeguard. If this fucks things up, here is a backup

--- activation_additions/hyperparameter_search.py
+++ activation_additions/hyperparameter_search.py
@@ -7,6 +7,10 @@ import numpy as np
 from functools import partial
 import torch
 from typing import List, Dict, Callable, Optional, Union, Tuple
+import matplotlib.pyplot as plt
+
+from matplotlib.colors import LinearSegmentedColormap
+
 
 from transformer_lens import HookedTransformer
 
@@ -114,3 +118,135 @@ def layer_coefficient_gridsearch(
     })
 
     return df
+
+
+def create_perplexity_matrices(df):
+    # Find unique values for layers and coefficients
+    layers = df['Layer'].unique()
+    coefficients = df['Coefficient'].unique()
+
+    # Initialize matrices to hold results
+    wanted_matrix = np.zeros((len(layers), len(coefficients)))
+    unwanted_matrix = np.zeros((len(layers), len(coefficients)))
+
+    # Iterate over all combinations of layer and coefficient
+    for i, layer in enumerate(layers):
+        for j, coefficient in enumerate(coefficients):
+            # Select rows with this combination of layer and coefficient
+            rows = df[(df['Layer'] == layer) & (df['Coefficient'] == coefficient)]
+            
+            # If any rows were found, compute averages and store in matrices
+            if not rows.empty:
+                wanted_matrix[i, j] = rows['Perplexity (wanted)'].mean()
+                unwanted_matrix[i, j] = rows['Perplexity (unwanted)'].mean()
+
+    return wanted_matrix, unwanted_matrix
+
+
+
+def pareto_frontier(Xs, Ys, maxX = True, maxY = True):
+    # Sort the list in ascending order of Xs
+    sorted_list = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+    pareto_front = [sorted_list[0]]
+    
+    # Loop through the sorted list
+    for pair in sorted_list[1:]:
+        if maxY: 
+            if pair[1] >= pareto_front[-1][1]: # Look for higher
+                pareto_front.append(pair)
+        else: 
+            if pair[1] <= pareto_front[-1][1]: # Look for lower
+                pareto_front.append(pair)
+                
+    return pareto_front
+
+
+def plot_pareto_frontier(wanted_matrix, unwanted_matrix):
+    # Flatten the matrices
+    wanted_points = wanted_matrix.flatten()
+    unwanted_points = unwanted_matrix.flatten()
+
+    # Get the Pareto frontier
+    pareto_points = pareto_frontier(wanted_points, unwanted_points, maxX = False, maxY = True)
+
+    # Separate X and Y coordinates for plotting
+    pareto_X = [point[0] for point in pareto_points]
+    pareto_Y = [point[1] for point in pareto_points]
+
+    # Create a mask for the Pareto frontier points
+    mask = np.isin(wanted_points, pareto_X) & np.isin(unwanted_points, pareto_Y)
+
+    # Create a linear segmented color map
+    cmap = LinearSegmentedColormap.from_list("mycmap", ["yellow", "purple"])
+
+    # Get the corresponding color for each point on the Pareto frontier
+    colors = cmap(np.linspace(0, 1, len(pareto_X)))
+
+    # Plot the original points and the Pareto frontier
+    fig, axs = plt.subplots(figsize=(7, 7))
+    axs.scatter(wanted_points[~mask], unwanted_points[~mask], color='grey', alpha=0.3)
+    sc = axs.scatter(pareto_X, pareto_Y, color=colors)
+    plt.xlabel('Wanted Perplexity')
+    plt.ylabel('Unwanted Perplexity')
+    plt.show()
+
+def plot_matrices_with_pareto(df, plot_pareto_points=True):
+    wanted_matrix, unwanted_matrix = create_perplexity_matrices(df)
+    
+    # Extract the layers and coefficients from the dataframe for tick labels
+    layers = df['Layer'].unique()
+    coefficients = df['Coefficient'].unique()
+
+    fig, axs = plt.subplots(ncols=2, figsize=(14, 6))
+
+    # Plot the wanted matrix
+    im1 = axs[0].imshow(wanted_matrix, cmap='Reds')
+    axs[0].set_title('Wanted Perplexities')
+    fig.colorbar(im1, ax=axs[0])
+
+    # Plot the unwanted matrix
+    im2 = axs[1].imshow(unwanted_matrix, cmap='Blues')
+    axs[1].set_title('Unwanted Perplexities')
+    fig.colorbar(im2, ax=axs[1])
+
+    # Set x and y ticks and labels for both matrices
+    for ax in axs:
+        ax.set_xticks(np.arange(len(coefficients)))
+        ax.set_yticks(np.arange(len(layers)))
+        ax.set_xticklabels(coefficients)
+        ax.set_yticklabels(layers)
+        ax.set_xlabel('Coefficient')
+        ax.set_ylabel('Layer')
+
+    if plot_pareto_points:
+        # Compute the Pareto frontier points
+        wanted_points = wanted_matrix.flatten()
+        unwanted_points = unwanted_matrix.flatten()
+        pareto_points = pareto_frontier(wanted_points, unwanted_points, maxX=False, maxY=True)
+
+        pareto_X_vals = [point[0] for point in pareto_points]
+        pareto_Y_vals = [point[1] for point in pareto_points]
+
+        # Find matrix indices of the Pareto points
+        pareto_indices = []
+
+        # Create a linear segmented color map
+        cmap = LinearSegmentedColormap.from_list("mycmap", ["yellow", "purple"])
+
+        # Get the corresponding color for each point on the Pareto frontier
+        colors = cmap(np.linspace(0, 1, len(pareto_X_vals)))
+
+        for x, y in zip(pareto_X_vals, pareto_Y_vals):
+            index = np.where((wanted_matrix == x) & (unwanted_matrix == y))
+            pareto_indices.append(index)
+
+        pareto_X_indices = [index[1][0] for index in pareto_indices]
+        pareto_Y_indices = [index[0][0] for index in pareto_indices]
+
+        # Overlay the Pareto frontier on both matrices
+        for ax in axs:
+            ax.scatter(pareto_X_indices, pareto_Y_indices, color=colors, s=50, label="Pareto Frontier")
+            ax.legend()
+
+    plt.tight_layout()
+    plt.show()
